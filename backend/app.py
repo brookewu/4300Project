6bbec3d20705a3f8d49a93d82be64e6505ca9765
Samantha Zhang <sjz46@cornell.edu>
2023-04-14 22:10:02 -0400

    compile local ver

--- backend/app.py
+++ backend/app.py
@@ -21,7 +21,7 @@ mysql_engine = MySQLDatabaseHandler(
 
 # Path to init.sql file. This file can be replaced with your own file for testing on localhost, but do NOT move the init.sql file
 # mysql_engine.load_file_into_db()
-db_path = os.path.join(os.environ['ROOT_PATH'],'restaurants.sql')
+db_path = os.path.join(os.environ['ROOT_PATH'],'restaurants_old.sql')
 mysql_engine.load_file_into_db(file_path = db_path)
 
 app = Flask(__name__)
@@ -106,4 +106,4 @@ def episodes_search():
     return sql_search(text)
 
 
-# app.run(debug=True)
+app.run(debug=True)
